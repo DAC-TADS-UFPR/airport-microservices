b7ae4553568153f6b138bff4959633bf1cc4e9e7
João Paes <126814093+xuizi@users.noreply.github.com>
2025-03-22 22:30:48 -0300

    Subindo MS do cliente e docker

---
+++ cliente-service/src/test/java/br/com/tads/dac/clienteservice/ClienteServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.tads.dac.clienteservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClienteServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
