0ee97af055c3e82f410b0290669287c12dd22304
eduardokuritza <eduardo.kuritza@gmail.com>
2025-03-26 12:12:53 -0300

    front-end

--- front-end/src/data/lib/getItem.ts
+++
@@ -1,7 +0,0 @@
-import { getPostType } from "../config/api";
-
-export default async function getItem(search: any, type: string, section?: string) {
-  const queryKey = ["key", search, section, type];
-  const data = await getPostType({ key: "key", queryKey });
-  return data;
-}
--- front-end/src/data/lib/getItems.ts
+++
@@ -1,7 +0,0 @@
-import { getPostsType } from "../config/api";
-
-export default async function getItems(params: any, type: string) {
-  const queryKey = ["key", params, type];
-  const data = await getPostsType({ key: "key", queryKey });
-  return data;
-}
