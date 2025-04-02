9acfec078985e060c26bb4b3e5de362788cdc5d1
maricotnicolas <nicolasmaricot@yahoo.fr>
2025-04-01 21:52:42 -0300

    Funcionalidade de busca de voos foi implementado

--- front-end/src/components/efetuar_reserva.js
+++ front-end/src/components/efetuar_reserva.js
@@ -13,3 +13,32 @@ export default function FlightReservation() {
     </div>
   );
 }
+const flights = [
+    { id: 1, origin: "GRU", destination: "JFK", date: "2025-04-10", time: "14:00", price: 500, miles: 25000 },
+    { id: 2, origin: "GRU", destination: "LAX", date: "2025-04-12", time: "10:00", price: 600, miles: 30000 },
+  ];
+  
+  export default function FlightReservation() {
+    const [origin, setOrigin] = useState("");
+    const [destination, setDestination] = useState("");
+    const [filteredFlights, setFilteredFlights] = useState([]);
+  
+    const handleSearch = () => {
+      setFilteredFlights(
+        flights.filter(
+          (f) =>
+            (!origin || f.origin.includes(origin.toUpperCase())) &&
+            (!destination || f.destination.includes(destination.toUpperCase()))
+        )
+      );
+    };
+  
+    return (
+      <div>
+        <input placeholder="Aeroporto Origem" value={origin} onChange={(e) => setOrigin(e.target.value)} />
+        <input placeholder="Aeroporto Destino" value={destination} onChange={(e) => setDestination(e.target.value)} />
+        <button onClick={handleSearch}>Buscar</button>
+      </div>
+    );
+  }
+  
