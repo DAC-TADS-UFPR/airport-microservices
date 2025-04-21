5e35841562b69a84b3e75a2bf69ac718240db053
eduardokuritza <eduardo.kuritza@gmail.com>
2025-04-21 13:34:19 -0300

    Criação de usuários

---
+++ front-end/src/data/config/user.ts
@@ -0,0 +1,24 @@
+import { TForm } from "@/hooks/useForm";
+
+import api from "./api";
+
+export async function createUser({ payload }: { payload: { form: TForm } }) {
+  const { name, email, cpf, complement, cep, city, street, neighborhood, number, state } = payload.form;
+  try {
+    const { data } = await api.post(`/client/create`, {
+      name: name.value,
+      email: email.value,
+      cpf: cpf.value,
+      complement: complement.value,
+      cep: cep.value,
+      city: city.value,
+      street: street.value,
+      neighborhood: neighborhood.value,
+      number: number.value,
+      state: state.value,
+    });
+    return data;
+  } catch (error: any) {
+    throw error;
+  }
+}
