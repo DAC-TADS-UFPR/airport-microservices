f9571b000977751e27421cde23bfbefae1fab451
eduardokuritza <eduardo.kuritza@gmail.com>
2025-06-26 08:36:28 -0300

    refactor: project folder

--- front-end/src/modules/not-found/SectionOne/SectionOne.tsx
+++ front-end/src/modules/not-found/SectionOne/SectionOne.tsx
@@ -11,7 +11,7 @@ const SectionOne: FC<SectionOneProps> = () => {
           <h1 className="block__status">404</h1>
           <p className="block__message">Oops! Não encontramos a página que você está procurando.</p>
           <button onClick={() => (window.location.href = "/")} title="VOLTAR PARA HOME" name="VOLTAR PARA HOME">
-            VOLTAR PARA HOME
+            VOLTAR PARA O DASHBOARD
           </button>
         </div>
       </div>
