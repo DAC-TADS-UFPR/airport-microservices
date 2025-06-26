cc593ce3dd0e4aa0300036084d3ce09024d71f9c
Maricot Nicolas <nicolasmaricot@yahoo.fr>
2025-06-25 23:19:36 -0300

    fix(R04) correçao  modelo Reserva  e validações

--- front-end/src/components/Ver_reserva.tsx
+++ front-end/src/components/Ver_reserva.tsx
@@ -1,6 +1,5 @@
-// models/reserva.js
-module.exports = (sequelize: { define: (arg0: string, arg1: { codigo: { type: any; allowNull: boolean; unique: boolean; }; dataHora: { type: any; allowNull: boolean; }; origem: { type: any; allowNull: boolean; }; destino: { type: any; allowNull: boolean; }; valorGastoReais: { type: any; allowNull: boolean; }; milhasGastas: { type: any; allowNull: boolean; }; estadoReserva: { type: any; allowNull: boolean; }; }) => any; }, DataTypes: { STRING: any; DATE: any; DECIMAL: (arg0: number, arg1: number) => any; INTEGER: any; ENUM: (arg0: string, arg1: string, arg2: string) => any; }) => {
-  const Reserva = sequelize.define('Reserva', {
+module.exports = (sequelize, DataTypes) => {
+  return sequelize.define('Reserva', {
     codigo: {
       type: DataTypes.STRING,
       allowNull: false,
@@ -31,20 +30,4 @@ module.exports = (sequelize: { define: (arg0: string, arg1: { codigo: { type: an
       allowNull: false,
     },
   });
-
-  return Reserva;
 };
-const { Sequelize, DataTypes } = require('sequelize');
-const sequelize = new Sequelize('nome_do_banco', 'usuario', 'senha', {
-  host: 'localhost',
-  dialect: 'mysql',
-});
-
-const Reserva = require('./models/reserva')(sequelize, DataTypes);
-
-sequelize.sync({ force: true }) // ou { alter: true } para manter dados
-  .then(() => {
-    console.log("Rserva criada!");
-  }).catch((err: any) => {
-    console.error("Reserva nao encontrada", err);
-  });
